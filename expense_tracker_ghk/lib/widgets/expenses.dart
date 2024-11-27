3ddb492b134a0b56bea9af646ed77abda05b29ca
gkolbeins <gkolbeins@gmail.com>
2024-11-27 21:32:59 +0000

    27. nóv

--- expense_tracker_ghk/lib/widgets/expenses.dart
+++ expense_tracker_ghk/lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 
@@ -28,6 +29,13 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context, 
+      builder: (ctx) => const NewExpense(),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +43,7 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Flutter expense tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
             ),
         ],
