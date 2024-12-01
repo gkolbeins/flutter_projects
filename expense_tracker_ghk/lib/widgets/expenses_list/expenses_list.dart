9869c6c4888b3aa71bfec15cf1b17d983805ff0d
gkolbeins <gkolbeins@gmail.com>
2024-12-01 15:00:01 +0000

    1. des
    villa löguð - verkefnið óklárað

--- expense_tracker_ghk/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker_ghk/lib/widgets/expenses_list/expenses_list.dart
@@ -7,9 +7,11 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key, 
     required this.expenses,
+    required this.onRemoveExpense,
     });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +19,9 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length, 
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]), 
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
         child: ExpensesItem(
           expenses[index],
           ),
